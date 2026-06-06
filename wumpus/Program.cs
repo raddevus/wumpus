fb362821f6a72087cb0acf93bc897f002301c501
raddevus <1radeutsch1@gmail.com>
2026-06-06 11:11:59 -0400

    Adding game loop so user can interact with it.

--- wumpus/Program.cs
+++ wumpus/Program.cs
@@ -11,3 +11,29 @@ if (args.Count() > 0){
    outerLayerSize = Convert.ToInt32(args[0]);
 }
 Game g = new(outerLayerSize);
+bool exitGame = false;
+
+while (!exitGame){
+   
+   var userChoice = GetUserChoice();
+   if (userChoice == -1){
+      Environment.Exit(0);
+   }
+
+
+}
+
+int GetUserChoice(){
+   Console.WriteLine("What would you like to do?");
+   Console.WriteLine("Q <ENTER>: Quit");
+   Console.WriteLine("Room Number: Move");
+   try {
+      var userInput = Console.ReadLine();
+      if (userInput.ToUpper() == "Q"){ return -1; } //quit
+      return Convert.ToInt32(userInput);
+   }
+   catch (Exception ex){
+      Console.WriteLine("Invalid input! Try again. Either Q or a Room number.");
+      return 0;
+   }
+}
